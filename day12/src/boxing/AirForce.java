06e2aee1fb28754afcce3636b6370d781ab795aa
Rpuplesun <tyu1300@naver.com>
2022-03-23 16:36:00 +0900

    dat12
    #내손을자바
    객체형변환 / instanceof 관련입니다.
    About Boxing / instanceof.

---
+++ day12/src/boxing/AirForce.java
@@ -0,0 +1,20 @@
+package boxing;
+
+public class AirForce extends Unit {
+
+
+	public AirForce(String name) {
+		super(name);
+	}
+	
+	@Override
+	public void attack() {
+		super.attack();
+		System.out.println(super.getName() + " >> 이륙");
+		System.out.println(super.getName() + " >> 공중공격 실행");
+	}
+
+	public void bombing() {
+		System.out.println(this.getName() + " >> 폭격");
+	}
+}
